b1f4add448ec40195868c486ae318e5a8e2ade63
Ahmed <ahmed.m.almousawi@gmail.com>
2024-02-05 22:19:05 -0700

    added some coding efficency

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,17 @@ input = pd.read_csv('ObesityDataSet_Regression.csv')
 # print(input['family_history_with_overweight'])
 output = input.copy()
 
-output['family_history_with_overweight'] = [1 if x == 'yes' else 0 for x in output['family_history_with_overweight']]
-output['Gender'] = [1 if x == 'Female' else 0 for x in output['Gender']]
+
+ones_col = ['family_history_with_overweight', 'Gender']
+ones = ['yes', 'Female']
+
+i=0
+for col in ones_col:
+    output[col] = [1 if x == ones[i] else 0 for x in output[col]]
+    i += 1
+
+
+# output['family_history_with_overweight'] = [1 if x == 'yes' else 0 for x in output['family_history_with_overweight']]
+# output['Gender'] = [1 if x == 'Female' else 0 for x in output['Gender']]
 print(output['family_history_with_overweight'])
 print(output['Gender'])

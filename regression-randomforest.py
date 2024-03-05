940a4cf404deedeb66baa75091831e90c552d5bc
al-ʿAnqāʾ <ahmed.m.almousawi@gmail.com>
2024-03-05 14:24:36 -0700

    added full model

--- regression-randomforest.py
+++ regression-randomforest.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split 
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 dataset = pd.read_csv('ProcessedObesityDataSet_Regression.csv')
+dataset = dataset.drop(index=0, axis=1)
 print(dataset.head())
 
 x = dataset.drop(['Weight'], axis=1)
@@ -13,3 +15,21 @@ print(x)
 print(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+regr = RandomForestRegressor(max_depth=20, random_state=0)
+regr.fit(x_train, y_train)
+
+y_train_pred = regr.predict(x_train)
+y_test_pred = regr.predict(x_test)
+
+r2_train = r2_score(y_train, y_train_pred)
+r2_test = r2_score(y_test, y_test_pred)
+
+print(f'The training accuracy for the model is {r2_train}')
+print(f'The testing accuracy for the model is {r2_test}')
+
+rmse_train = (mean_squared_error(y_train, y_train_pred))**(1/2)
+rmse_test = (mean_squared_error(y_test, y_test_pred))**(1/2)
+
+print(f'The training RMSE for the model is {rmse_train}')
+print(f'The testing RMSE for the model is {rmse_test}')

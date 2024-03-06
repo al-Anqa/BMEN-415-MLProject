a7bfd2bd65becac200d4800ba13710fd7101eaa0
al-ʿAnqāʾ <ahmed.m.almousawi@gmail.com>
2024-03-06 12:56:16 -0700

    started work on classification

---
+++ classification-gaussiannaivebayes.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import r2_score, mean_squared_error
+from common import classification_data
+
+x, y = classification_data()
+
+print(x)
+print(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+regr = RandomForestRegressor(max_depth=10, random_state=0)
+regr.fit(x_train, y_train)
+
+y_train_pred = regr.predict(x_train)
+y_test_pred = regr.predict(x_test)
+
+r2_train = r2_score(y_train, y_train_pred)
+r2_test = r2_score(y_test, y_test_pred)
+
+print(f'The training accuracy for the model is {r2_train}')
+print(f'The testing accuracy for the model is {r2_test}')
+
+rmse_train = (mean_squared_error(y_train, y_train_pred))**(1/2)
+rmse_test = (mean_squared_error(y_test, y_test_pred))**(1/2)
+
+print(f'The training RMSE for the model is {rmse_train}')
+print(f'The testing RMSE for the model is {rmse_test}')

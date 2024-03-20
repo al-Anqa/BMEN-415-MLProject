177d16699426256f0a03991b6fc86f9162b10de6
al-ʿAnqāʾ <ahmed.m.almousawi@gmail.com>
2024-03-20 12:09:10 -0600

    added visualization plots

--- regression-votingregressor.py
+++ regression-votingregressor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.cross_decomposition import PLSRegression
@@ -37,3 +38,35 @@ rmse_test = (mean_squared_error(y_test, y_test_pred))**(1/2)
 
 print(f'The training RMSE for the model is {rmse_train}')
 print(f'The testing RMSE for the model is {rmse_test}')
+
+plt.subplot(1, 2, 1)
+plt.scatter(y_train, y_train_pred, c='indigo', alpha=0.3, label='Training Data')
+
+plt.yscale('log')
+plt.xscale('log')
+
+p1 = max(max(y_train_pred), max(y_train))
+p2 = min(min(y_train), min(y_train))
+plt.plot([p1, p2], [p1, p2], 'b-')
+plt.xlabel('True Values', fontsize=15)
+plt.ylabel('Predicted', fontsize=15)
+plt.title("Random Forest Regression: Training True Values vs Predicted ")
+plt.axis('equal')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.scatter(y_test, y_test_pred, c='steelblue', label='Test Data')
+
+plt.yscale('log')
+plt.xscale('log')
+
+p1 = max(max(y_test_pred), max(y_test))
+p2 = min(min(y_test), min(y_test))
+plt.plot([p1, p2], [p1, p2], 'b-')
+plt.xlabel('True Values', fontsize=15)
+plt.ylabel('Predicted', fontsize=15)
+plt.title("Random Forest Regression: Testing True Values vs Predicted ")
+plt.axis('equal')
+plt.legend()
+
+plt.show()
